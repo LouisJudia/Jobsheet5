7e817f6495da63cad5893a6afff0bed4dd723c97
Louis Judia <louisjudia06@gmail.com>
2024-09-30 20:22:26 +0700

    Sesudah

--- PemilihanBilangan08.java
+++ PemilihanBilangan08.java
@@ -3,18 +3,14 @@ import java.util.Scanner;
 public class PemilihanBilangan08 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         System.out.print("Masukkan sebuah angka : ");
-        int angka = sc.nextInt();
+        int angka = in.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String tenary = (angka % 2== 0) ? "genap" : "ganjil";
+        System.out.printf("Angka %s termasuk bilangan %s" , angka ,tenary);
+        
+        in.close();   
     }
 }
